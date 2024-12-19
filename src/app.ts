a46b87b95113b998cf00b4e0711d520bac3bab71
William Shaka <williamkavingo@gmail.com>
2024-12-19 05:29:53 +0300

    Interacting with DOM elements

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,49 @@
-class ProjectInput{
-templateElement: HTMLTemplateElement;
-hostElement: HTMLDivElement;
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
+	constructor() {
+		this.templateElement = <HTMLTemplateElement>(
+			document.getElementById('project-input')!
+		);
+		this.hostElement = <HTMLDivElement>document.getElementById('app')!;
 
-    constructor(){
-        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!;
-        this.hostElement = 
-    }
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+		this.element = <HTMLFormElement>importedNode.firstElementChild!;
+		this.element.id = 'user-input';
+
+		this.titleInputElement = <HTMLInputElement>(
+			this.element.querySelector('#title')!
+		);
+		this.descriptionInputElement = <HTMLInputElement>(
+			this.element.querySelector('#description')!
+		);
+		this.peopleInputElement = <HTMLInputElement>(
+			this.element.querySelector('#people')!
+		);
+		this.configure();
+		this.attach();
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		console.log(this.titleInputElement.value);
+	}
+
+	private configure() {
+		this.element.addEventListener('submit', this.submitHandler.bind(this));
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement('afterbegin', this.element);
+	}
 }
+
+const prjInput = new ProjectInput();
